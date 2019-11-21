dbc50d7b42ad1ee7fdc0bd918a46e68fca8dc892
miyamo93 <miyamo0802@gmail.com>
2019-11-21 21:59:37 +0900

    更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'tweets#index'
+  get  'image' => 'tweets#image'
   resources :tweets do
     resources :comments, only: [:create]
   end
